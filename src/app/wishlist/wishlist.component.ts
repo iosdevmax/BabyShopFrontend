0fe2fbf2c4a2d91db15a560a8b3cd36079643613
Syngmaster <max6361@mail.ru>
2019-06-26 21:16:21 +0100

    removing items from wishlist

--- src/app/wishlist/wishlist.component.ts
+++ src/app/wishlist/wishlist.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {DataService} from '../Services/data.service';
+import {SharedService} from '../Services/shared.service';
+import {Item} from '../Models/item.model';
 
 @Component({
   selector: 'app-wishlist',
@@ -7,9 +10,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WishlistComponent implements OnInit {
 
-  constructor() { }
+  wishlistItems: Array<Item>;
+
+  constructor(private data: DataService,
+              private shared: SharedService) {
+    this.data.retrieve_wishlisted_items().subscribe(items => {
+      this.wishlistItems = items;
+      // updating local storage with up-to-date info
+      localStorage.setItem('wishlist', JSON.stringify(this.wishlistItems.length));
+      this.shared.changeWishlistValue(this.wishlistItems.length);
+    });
+  }
 
   ngOnInit() {
+
+  }
+
+  removeFromWishlist(item: Item, index: number) {
+    this.data.add_remove_to_wishlist(item).subscribe(result => {
+      // if successfully removed, updating top view and local storage
+      const isAdded = result['wishlist'];
+      if (isAdded === false) {
+        this.updateWishlistCount(isAdded);
+        this.wishlistItems = this.wishlistItems.filter( element => {
+          return element.id !== item.id;
+        });
+      }
+
+    });
+  }
+
+  private updateWishlistCount(isAdded: boolean) {
+    // getting current wishlist count and updating it
+    let currentCount = JSON.parse(localStorage.getItem('wishlist')) as number;
+    if (isAdded) {
+      currentCount = currentCount + 1;
+    } else {
+      if (currentCount >= 1) {
+        currentCount = currentCount - 1;
+      }
+    }
+
+    // updating local storage with the result and wishlist count icon
+    localStorage.setItem('wishlist', JSON.stringify(currentCount));
+    this.shared.changeWishlistValue(currentCount);
   }
 
 }
