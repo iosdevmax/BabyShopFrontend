6302effb648e116ceb92555be0045dc6d360c996
Syngmaster <max6361@mail.ru>
2019-07-12 07:51:19 +0100

    cart items checkout

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -12,4 +12,14 @@ export class CheckoutComponent implements OnInit {
   ngOnInit() {
   }
 
+  handleChange(evt) {
+    const buttonId = evt.target.id;
+    const total = document.getElementById('shipping-amount');
+    if (buttonId === 'standard') {
+      total.innerText = '€5.00';
+    } else {
+      total.innerText = '€10.00';
+    }
+  }
+
 }
